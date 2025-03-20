6e00e776ad7a8a6dc260aa7f22bb5d554ffb5ddf
Javier Ibañez Vizuete <javieriibanezvizuete@gmail.com>
2025-03-20 14:37:23 +0100

    add link to informatica page on button while clicking

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+const changepage = () => {
+	const inputName = document.querySelector(".input-name");
+	console.log(inputName);
+	const btnLogin = document.querySelector(".continue-btn");
+	btnLogin.addEventListener("click", (event) => {
+        if (!inputName.value.trim()) {
+            event.preventDefault();
+            return alert("Por favor introduzca un nombre para iniciar sesion");
+        }
+        window.location.href = "/pages/informatica.html"
+	});
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-    
+	changepage();
 });
